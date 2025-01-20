5dc4df5ddef81dfa64a45c3ea72d743f5a8cdca1
MIYA\emeie <2400801@systex.com.tw>
2025-01-20 11:41:17 +0800

    Initial commit

--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WordTester.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WordTester.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c7128b01d5b5f114fdfb458d66a7050578f305")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
